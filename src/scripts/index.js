4560ff6054493b6b1e2b096a23564d8bd0efe2e6
Pepe <pepe@insterior.co.kr>
2021-07-29 14:04:28 +0900

    feat: html script. style add

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -16,9 +16,32 @@ const commonJS = (_ => {
     return el;
   };
 
+  const ELEMENT = {
+    INPUT_FILE: '[name="file"]',
+    PATH_EL   : 'sheet-path'
+  };
+
+  const {INPUT_FILE, PATH_EL} = ELEMENT;
+
+  const bindFilePath = _ => {
+    const setFilePath = e => {
+      console.log(e.target)
+      const fileName = e.target.files[0].name;
+      const pathEl = docSelector({el: `.${PATH_EL}`})
+      pathEl.textContent = fileName;
+    };
+
+    const getFilePath = _ => {
+      const inputFile = docSelector({el: INPUT_FILE});
+      inputFile.addEventListener('change', setFilePath);
+    };
+
+    getFilePath();
+  };
+
   const init = _ => {
     window.addEventListener('touchstart', _ => {});
-    console.log('init');
+    bindFilePath();
   };
 
   return {
